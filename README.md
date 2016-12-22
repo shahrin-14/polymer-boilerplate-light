f51378dbe561abb32dec547018080e23a09e4250
unknown <SHAHRSH3@PHMYSA-L70036.ap.novartis.net>
2016-12-22 14:45:29 +0800

    Adding dependency to Bower

--- README.md
+++ README.md
@@ -1,51 +1,40 @@
 # Polymer Boilerplate Light
 
-This is a boilerplate for [Polymer](http://www.polymer-project.org/) 2.0 with minimal tools and dependencies for people behind proxies and firewalls in a locked computer.
+This is a boilerplate for [Polymer](http://www.polymer-project.org/) 2.0 with minimal tools and dependencies. For some reason or another, I just could not get Polymer-CLI working, but I suspect its because I am behind proxies and firewalls and using a locked computer.
+I tried to live without Bower, but packages on NPM are of a different version than on Github, and it was just easier to install dependent web components using Bower.
+
+Please be advised:
+
+* If you are able to use Polymer-Cli, I suggest you do. 
+* This is not for production use. It is just for those who want to try out web components.
 
 ## Features
 
-* Minimal tools. Only used NPM and Gulp. No Bower or Polimer-CLI.
+* Minimal tools. Only used NPM, Bower and Gulp. No Polimer-CLI.
+* Can use ES6 Javascript, which is later transpiled by Babel
 
 ## Prerequisites
-We only need [Gulp](http://gulpjs.com/) to be installed globally.
+We need [Bower](https://bower.io/) and [Gulp](http://gulpjs.com/) to be installed globally.
 
 ```
+#Run the following to install bower globally
+npm install -g bower
 #Run the following to install gulp globally
 npm install -g gulp
 #Run this line to install all project specific dependent items
 npm install
+#Run this line to install all project specific dependent components
+bower install
 ```
-
-## Project Structure
-All code that is used for development is in the 'src' folder. There we have 2 main folders:
-
-*   CustomComponents
-
- Items in this folder will go through the build process
-
-*   StaticComponents
-
- Components that you get online, which you want to make a reference to will be placed here. See [Pre Build](#pre-build).
- 
-## Pre Build
-Run `gulp initialize-ExternalResources`
-This task downloads all external resources required into specified location (See the ExternalResources array in gulpfile.js).
-Do change the following code accordingly if you are behind a proxy.
-```
-return download(resource.url, {
-              //proxy : "http://<my-proxy-server>:<port-number>"
-            })
-```
-Run `gulp build-ExternalResources`
-This task copies items from src/StaticComponents to dist/StaticComponents
 
 ## Build
 Run `gulp build`
 
 This task performs the following:
 
-*   Cleans the dist folder except for dist/StaticComponents
-*   Copies all corresponding files into the dist folder except for dist/CustomComponents (which will go through the build process below) and dist/StaticComponents (which is manually maintained in the Pre Build step above)
+*   Cleans the dist folder
+*   Copies all files in bower_components to dist/bower_components
+*   Copies all corresponding files into the dist folder except for dist/components (which will go through the build process below)
 *   HTML files in folder src/CustomComponents will be separated into HTML and JS files using crisper
 *   ES6 JS syntax will be transformed using Babel
 *   Specified files will be aggregated for optimization using Vulcanize
@@ -53,3 +42,6 @@ This task performs the following:
 ## Run
 Run `gulp serve`
 Then open a browser and log on to [http://localhost:3000/](http://localhost:3000/)
+
+## Deploy
+Just copy all the files you want in the generated dist folder.
--- gulpfile.js
+++ gulpfile.js
@@ -1,53 +1,56 @@
 var gulp = require('gulp')
 
 //var request = require('request')
-var vulcanize = require('gulp-vulcanize')
-var crisper = require('gulp-crisper')
-var babel = require('gulp-babel')
-var serve = require('gulp-serve')
+
 var clean = require('gulp-clean')
 var tap = require('gulp-tap')
 
 gulp.task('clean', function () {
-    //This cleans out all the contents of the Dist folder except for the StaticComponents folder
-    return gulp.src(['dist/*','!dist/StaticComponents'], {read: false})
+    //This cleans out all the contents of the dist folder
+    return gulp.src('dist/*', {read: false})
         .pipe(tap(function(file,t) { console.log(file.path) }))
         .pipe(clean());
 });
-gulp.task('cleanCopy',['clean'],function(){
-    //This will copy anything in the src folder except for CustomComponents folder and StaticComponents folder
+gulp.task('copyComponents',['clean'],function(){
+    //This will copy anything in the src folder except for the components folder
     //The custom components will be built by Babel
-    return gulp.src(['src/**/*', '!src/CustomComponents/**/*','!src/StaticComponents/**/*'])
+    return gulp.src(['src/**/*', '!src/components/**/*'])
         .pipe(tap(function(file,t) { console.log(file.path) }))
         .pipe(gulp.dest('dist'));
 })
 
-gulp.task('crisper',['cleanCopy'], function() {
-  return gulp.src('src/CustomComponents/*.html')
-    .pipe(tap(function(file,t) { console.log(file.path) }))
-    .pipe(crisper({
-        scriptInHead: false, // true is default 
-        onlySplit: false
-    }))
-    .pipe(gulp.dest('dist/CustomComponents'));
+gulp.task('copyBower',['clean'],function(){
+    return gulp.src(['bower_components/**/*'])
+        .pipe(gulp.dest('dist/bower_components'));
+})
+
+gulp.task('crisper',['copyComponents','copyBower'], function() {
+    var crisper = require('gulp-crisper')
+    return gulp.src('src/components/*.html')
+        .pipe(tap(function(file,t) { console.log(file.path) }))
+        .pipe(crisper({
+            scriptInHead: false, // true is default 
+            onlySplit: false
+        }))
+        .pipe(gulp.dest('dist/components'));
 });
 
-var babeilfy = 
-
 gulp.task('babelify',['crisper'], function() {
-    return gulp.src('dist/CustomComponents/*.js')
-    .pipe(tap(function(file,t) { console.log(file.path) }))
-    .pipe(babel({
-        presets: ['es2015']
-    }))
-    .pipe(gulp.dest('dist/CustomComponents'));
+    var babel = require('gulp-babel')
+    return gulp.src('dist/components/*.js')
+        .pipe(tap(function(file,t) { console.log(file.path) }))
+        .pipe(babel({
+            presets: ['es2015']
+        }))
+        .pipe(gulp.dest('dist/components'));
 });
 
 gulp.task('vulcanize',['crisper','babelify'], function() {
     //Vulcanize selected files for optimization
-    return gulp.src(['dist/CustomComponents/icon-toggle.html'])
+    var vulcanize = require('gulp-vulcanize')
+    return gulp.src(['dist/components/icon-toggle.html'])
         .pipe(vulcanize({
-            excludes: ['dist/StaticComponents/polymer/polymer.html'],
+            excludes: ['dist/bower_components/polymer/polymer.html'],
             stripExcludes: false,
             stripComments : true,
             inlineScripts: true
@@ -56,7 +59,7 @@ gulp.task('vulcanize',['crisper','babelify'], function() {
             //stripExcludes: false,
             //inlineScripts: false
         }))
-        .pipe(gulp.dest('dist/CustomComponents'))
+        .pipe(gulp.dest('dist/components'))
         .pipe(tap(function(file,t) {
             //Remove associating javascript file
             var javascriptPath = file.path.replace('.html','.js')
@@ -66,66 +69,4 @@ gulp.task('vulcanize',['crisper','babelify'], function() {
 });
 
 gulp.task('build', ['crisper','babelify','vulcanize']);
-gulp.task('serve', serve({root: ['dist']}));
-
-/*
-Gulp tasks below are only used to maintain and build the StaticComponents folder
-*/
-
-
-var download = require('gulp-download-stream')
-var ExternalResources = [
-    { 
-        url : 'https://polygit.org/components/polymer/polymer.html', 
-        dest : "src/StaticComponents/polymer" 
-    },{ 
-        url : 'https://polygit.org/components/polymer/polymer-mini.html', 
-        dest : "src/StaticComponents/polymer" 
-    },{ 
-        url : 'https://polygit.org/components/polymer/polymer-micro.html', 
-        dest : "src/StaticComponents/polymer" 
-    },{
-        url : 'https://raw.githubusercontent.com/PolymerElements/iron-flex-layout/master/iron-flex-layout.html', 
-        dest : "src/StaticComponents/iron-flex-layout" 
-    },{
-        url : 'https://raw.githubusercontent.com/PolymerElements/iron-icon/master/iron-icon.html', 
-        dest : "src/StaticComponents/iron-icon" 
-    },{
-        url : 'https://raw.githubusercontent.com/PolymerElements/iron-icons/master/iron-icons.html', 
-        dest : "src/StaticComponents/iron-icons" 
-    },{
-        url : 'https://raw.githubusercontent.com/PolymerElements/iron-iconset-svg/master/iron-iconset-svg.html', 
-        dest : "src/StaticComponents/iron-iconset-svg" 
-    },{
-        url : 'https://raw.githubusercontent.com/PolymerElements/iron-meta/master/iron-meta.html', 
-        dest : "src/StaticComponents/iron-meta" 
-    },{
-        url : 'https://raw.githubusercontent.com/PolymerElements/iron-meta/master/iron-meta.html', 
-        dest : "src/StaticComponents/iron-meta" 
-    },{
-        url : 'https://polygit.org/components/webcomponentsjs/webcomponents-lite.min.js', 
-        dest : "src/StaticComponents/webcomponentsjs" 
-    }
-  ]
-
-ExternalResources = ExternalResources.map(function(resource){ 
-    gulp.task('download-'+resource.url,function(res){
-        return download(resource.url, {
-                //proxy : "http://<my-proxy-server>:<port-number>"
-            })
-            .pipe(gulp.dest(resource.dest));
-            
-    })
-    return 'download-'+resource.url
-})
-gulp.task('initialize-ExternalResources',ExternalResources)
-
-gulp.task('build-ExternalResources',function(){
-    //This will copy anything in the src folder except for CustomComponents folder and StaticComponents folder
-    //The custom components will be built by Babel
-    return gulp.src(['src/StaticComponents/**/*'])
-        .pipe(tap(function(file,t) { console.log(file.path) }))
-        .pipe(gulp.dest('dist/StaticComponents'));
-})
-
-//https://github.com/PolymerElements/iron-flex-layout/archive/v1.3.1.zip
+gulp.task('serve', require('gulp-serve')({root: ['dist']}));
